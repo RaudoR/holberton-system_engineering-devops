5c4f5d0a02ffc2cd04433e2edaa6335c59233c91
RaudoR <rivaspaulino@outlook.com>
2020-05-04 18:48:12 +0000

    get todo list of emp

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''script to get Emp ID and TODO list from REST API'''
+
+if __name__ =="__main__":
+    import requests as re
+    from sys import argv
+
+    userId = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users"
+    user = re.get(url + '/{}'.format(userId)).json()
+    todo = re.get(url + '/{}/todos'.format(userId)).json()
+    complete = []
+
+    for task in todo:
+        if task.get('completed') is True:
+            complete.append(task.get('title'))
+
+    print('Employee {} is done with tasks({}/{}):'.format
+          (user.get('name'), len(complete), len(todo)))
+    for task in complete:
+        print('\t {}'.format(task))
